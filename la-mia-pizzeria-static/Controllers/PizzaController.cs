0c5c1a988dcfda693f115211739d74161d40f8a7
Giadagenovesi <giada.g@outlook.it>
2023-09-29 14:32:00 +0200

    Creata Database e PizzaController, popolate pagine Index e UserIndex dinamicamente utilizzando i model

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,29 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+
+                return View("Index", pizzas);
+            }
+        }
+
+        public IActionResult UserIndex()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+
+                return View("UserIndex", pizzas);
+            }
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Immagine { get; set; }
+        public string Gusto {  get; set; }
+        public string Ingredienti { get; set; } 
+        public float Prezzo { get; set; }
+
+        public Pizza(string immagine, string gusto, string ingredienti, float prezzo)
+        {
+            this.Immagine = immagine;
+            this.Gusto = gusto;
+            this.Ingredienti = ingredienti;
+            this.Prezzo = prezzo;   
+        }
+    }
+}
